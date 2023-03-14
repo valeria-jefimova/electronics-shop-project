a0c671ca0d146efed03fb3be791b342be3bb331a
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-03-14 19:42:20 +0200

    fix test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item1():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price()==200000
+
