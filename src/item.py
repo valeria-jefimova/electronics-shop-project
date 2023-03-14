dfe9350731c3adfa5ad82f1d5e6394c06f2f1649
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-03-14 21:39:02 +0200

    fix ite.py file add pyproject.toml

--- src/item.py
+++ src/item.py
@@ -17,6 +17,8 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        Item.all.append(self)
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
