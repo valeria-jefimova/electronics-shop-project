e7aafb2677438dc1fddaf508020019263490c1b1
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-03-21 21:35:35 +0200

    updated item, main, test_item

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -8,30 +11,53 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+        self.__name = name
+        self.price = float(price)
+        self.quantity = int(quantity)
+        self.all.append(self)
 
-        Item.all.append(self)
+    @property  #getter
+    def name(self):
+        return self.__name
+
+    @name.setter  #setter
+    def name(self, name: str):
+        if len(self.__name) <= 10:
+            self.__name = name
+        else:
+            raise ValueError("Длина наименования товара превышает 10 символов")
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity
+        return int(self.price) * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = (self.price * Item.pay_rate)
-        return self.price
+        self.price = int(self.price * self.pay_rate)
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """Класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv"""
+        with open('./src/items.csv', newline='', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+                print(row)
+
+    @staticmethod
+    def string_to_number(value: str) -> int:
+        """Статический метод, возвращающий число из числа-строки"""
+        return round(int(value[0]), 2)
+
+
 
 
--- tests/test_item.py
+++ tests/test_item.py
@@ -5,16 +5,42 @@ from src.item import Item
 
 
 @pytest.fixture
-def item1():
+def item():
     return Item("Смартфон", 10000, 20)
 
 
-def test_calculate_total_price(item1):
-    assert item1.calculate_total_price() == 200000
+def test_item_init(item):
+    """
+    Тест инициации класса Item
+    """
+    assert item.name == 'Смартфон'
+    assert item.price == 10000
+    assert item.quantity == 20
+    assert isinstance(item.name, str)
+    assert isinstance(item.price, float)
+    assert isinstance(item.quantity, int)
 
 
-def test_apply_discount(item1):
-    item1.pay_rate = 1.0
-    item1.apply_discount()
-    assert item1.price == 10000
+def test_calculate_total_price(item):
+    """
+    Тест расчета общей стоимости конкретного товара
+    """
+    assert item.calculate_total_price() == 200000
 
+
+def test_apply_discount(item):
+    """
+    Тест расчета скидки конкретного товара
+    """
+    item.pay_rate = 1.0
+    item.apply_discount()
+    assert item.price == 10000
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 8
+
+
+def test_string_to_number():
+    assert Item.string_to_number('56.879') == 5
